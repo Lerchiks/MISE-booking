8657503926783f975e2878c7dd34390fd4c9a146
Valeria Dubinina <zebe16027@gmail.com>
2026-09-05 19:21:24 +0300

    app fixed (added suspense)

--- mise-booking/src/app/booking-confirmation/page.tsx
+++ mise-booking/src/app/booking-confirmation/page.tsx
@@ -1,5 +1,10 @@
 import { BookingConfirmationPage } from '@/views/booking-confirmation-page';
+import { Suspense } from 'react';
 
 export default function Home() {
-  return <BookingConfirmationPage />;
+  return (
+    <Suspense fallback={<div>Загрузка...</div>}>
+      <BookingConfirmationPage />
+    </Suspense>
+  );
 }
